1f4ff28e631e5e670a2b8abdb220ee039383fb85
suphaloek66 <suphaleok_khueanphet@cmu.ac.th>
2023-12-12 16:26:27 +0700

    complete_All

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age,h,b;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=25){
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<100){
+            cout << "Your character = Chopper";
+        }else if(h<180){
+            cout << "Your character = Usopp";}
+        else{
+        cout <<"Enter your bounty: ";
+        cin >> b;
+        if(b>1100000000){
+            cout << "Your character = Zoro";
+        }else{
+            cout << "Your character = Sanji";}}
+    
+    }else if(age<=60){
+        cout <<"Enter your bounty: ";
+        cin >> b;
+        if(b>500000000){
+        cout << "Your character = Jinbe";
+        }else{
+        cout << "Your character = Franky";}
+    }else{
+            cout << "Your character = Brook";
+    }}
+
+
+//"Zoro"
+//"Sanji"
+//"Usopp"
+//"Chopper"
+//"Franky"
+//"Brook"
+//"Jinbe"
+
